668ab5412a00ea766671e396c71092ff2fb5b047
Haginus <hagiandrei.ah@gmail.com>
2022-01-18 22:41:32 +0200

    task form array

--- frontend/src/app/pages/monitor/monitor.component.ts
+++ frontend/src/app/pages/monitor/monitor.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-monitor',
   templateUrl: './monitor.component.html',
   styleUrls: ['./monitor.component.scss']
 })
-export class MonitorComponent implements OnInit {
+export class MonitorComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
+  sendRequestForm = new FormGroup({
+    tasks: new FormArray([]),
+  });
+
+  @ViewChild('container') container!: ElementRef<HTMLInputElement>;
+
+  rowHeigth: number = 0;
+
   ngOnInit(): void {
   }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.onResize();
+    }, 0);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.rowHeigth = this.container.nativeElement.clientHeight / 2;
+  }
+
+  get taskFormArray() {
+    return this.sendRequestForm.get("tasks") as FormArray;
+  }
+
+  addTask() {
+    const task = new FormGroup({
+      duration: new FormControl(null, [Validators.required])
+    });
+    this.taskFormArray.push(task);
+  }
+
+  
 
 }
